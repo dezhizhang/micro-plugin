b40706a4f8d3602200a8394c7ef3bdb2c1a463d8
dezhizhang <1541609448@qq.com>
2022-05-15 16:28:46 +0800

    feat:加载应用

---
+++ src/applications/app.helpers.js
@@ -0,0 +1,35 @@
+// 描述应用的整个状态
+
+// 应用初始化
+export const NOT_LOADED = 'NOT_LOADED';
+// 加载资源
+export const LOADING_SOURCE_CODE = 'LOADING_SOURCE_CODE';
+// 应用还没有调用bootstrap
+export const NOT_BOOTSTRAPPED = 'NOT_BOOTSTRAPPED';
+//启动中
+export const BOOTSTRAPPING = 'BOOTSTRAPPING';
+// 没有调用mount方法
+export const NOT_MOUNTED = 'NOT_MOUNTED';
+// 正在挂载中
+export const MOUNTING = 'MOUNTING';
+// 更新中
+export const UPDATINMG = 'UPDATINMG';
+//自裁中
+export const UNMOUNTING = 'UNMOUNTING';
+// 完全绡
+export const UNLOADING = 'NLOADING';
+// 文件加载失败
+export const LOAD_ERR = 'LOAD_ERR';
+// 代码出错
+export const SKIP_BECAUSE_BROKEN = 'SKIP_BECAUSE_BROKEN';
+
+// 当前应用是否被激活
+export function isAactive(app) {
+    app.status === NOT_MOUNTED
+}
+
+// 当前应用是否要被激活
+export function shouldBeActive(app) {
+    return app.activeWhen(window.location);
+}
+
--- src/applications/app.js
+++ src/applications/app.js
@@ -2,10 +2,24 @@
 
 /**
  * @param {*} appName 应用名称
- * @param {*} 
+ * @param {*} loadApp 加载应用
+ * @param {*} activeWhen 当前激活的应用
+ * @param {*} customProps 自定义属性
 */
 
-export function registerApplication() {
+import { NOT_LOADED } from "./app.helpers";
 
+// 用来存放所有应用
+const app = [];
+
+export function registerApplication(appName,loadApp,activeWhen,customProps) {
+    app.push({
+        name:appName,
+        loadApp,
+        activeWhen,
+        customProps,
+        status:NOT_LOADED
+    });
+    
 }
 
